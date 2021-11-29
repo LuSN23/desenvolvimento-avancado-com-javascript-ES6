1f3041b003218e0b4435ee7012ceef83a1a9cfb6
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-11-28 23:40:14 -0300

    Arrow Functions - Comparando função clássica vs arrow function

--- Aula01_Funcoes-Avancadas-do-ES6/arrow-functions.js
+++ Aula01_Funcoes-Avancadas-do-ES6/arrow-functions.js
@@ -1,4 +1,4 @@
-//Arrow Functions
+//Arrow Functions:
 
 //Função clássica 
 function log(value) {
@@ -24,4 +24,29 @@ log('test');
 2 maneiras de usá-las:
         1 - Atribuindo a uma variável
         2 - Passando-as como argumento de outra função(funções de primeira-classe)
+*/
+
+//Surgimento das 'Arrow Functions' com o ES6
+//Função sum(sumOld) da maneira clássica
+var sumOld = function(a, b) {
+    return a + b;
+};
+
+console.log(sumOld(5, 20));
+
+//Função sum com Arrow Function 
+var sum = (a, b) => a + b;   //Omitindo return(apenas uma expressão)
+
+console.log(sum(5, 15));
+
+/*
+- Obrigatoriamente usar os caractéres =>
+- São funções anônimas então tem que ser atríbuidas à variáveis ou inseridas como argumentos 
+de funções.
+- Tem diferentes maneiras de se escrever.
+
+//Passos:
+- Omitir a palavra function.
+- Parênteses(na maioria dos casos) para os argumentos.
+- E o retorno implícito no lado direito.
 */
