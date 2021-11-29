3730af03b23bf9913b6254c22f624e3a56fb709b
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-11-28 23:20:47 -0300

    Arrow Functions - Função Clássica

---
+++ Aula01_Funcoes-Avancadas-do-ES6/arrow-functions.js
@@ -0,0 +1,16 @@
+//Arrow Functions
+
+//Função clássica 
+function log(value) {
+    console.log(value);
+}
+
+log('test');
+
+/*
+- palavra function
+- nome para a função(nome opcional)
+- parênteses, dentro os argumentos da função
+*/
+
+
