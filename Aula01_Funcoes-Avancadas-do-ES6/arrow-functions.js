0e0a6e96e200e18d559a854074f81ce1af8f0833
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-11-28 23:26:44 -0300

    Arrow Functions - Função Anônima

--- Aula01_Funcoes-Avancadas-do-ES6/arrow-functions.js
+++ Aula01_Funcoes-Avancadas-do-ES6/arrow-functions.js
@@ -13,4 +13,15 @@ log('test');
 - parênteses, dentro os argumentos da função
 */
 
+//Função anônima
+//Atribuindo a uma variável:
+var log = function(value) {
+    console.log(value);
+};
 
+log('test');
+/*
+2 maneiras de usá-las:
+        1 - Atribuindo a uma variável
+        2 - Passando-as como argumento de outra função(funções de primeira-classe)
+*/
