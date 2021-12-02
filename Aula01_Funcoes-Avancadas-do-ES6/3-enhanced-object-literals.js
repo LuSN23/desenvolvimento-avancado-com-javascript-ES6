641f67544abf0030dd7740e38410519a101db494
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-12-02 17:26:40 -0300

    Enhanced Object Literals - Atribuindo valores para propriedades e métodos fazendo referência a uma variavel no modelo clássico

--- Aula01_Funcoes-Avancadas-do-ES6/3-enhanced-object-literals.js
+++ Aula01_Funcoes-Avancadas-do-ES6/3-enhanced-object-literals.js
@@ -8,3 +8,15 @@ var obj = {
 - Abre-se chaves
 - Dentro dessas chaves é possível declarar propriedades e métodos
 */
+
+
+
+//Outra maneira de atribuir valores para propriedades e métodos, na maneira clássica
+//Referenciando uma variável
+var prop1 = 'Digital Innovation One';
+
+var obj = {
+    prop1: prop1  //Fazendo referência à variável prop1 ao criar a propriedade prop1
+};
+
+console.log(obj); // { prop1: 'Digital Innovation One' }
