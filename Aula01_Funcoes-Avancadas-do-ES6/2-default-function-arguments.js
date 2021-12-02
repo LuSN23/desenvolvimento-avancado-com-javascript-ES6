ca5d1ddf3c1e510a8f71fb60005bf145b2dce969
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-12-01 22:41:39 -0300

    Default Function Arguments - Lidando com parâmetros - Esquecendo de passar um parâmetro

--- Aula01_Funcoes-Avancadas-do-ES6/2-default-function-arguments.js
+++ Aula01_Funcoes-Avancadas-do-ES6/2-default-function-arguments.js
@@ -6,3 +6,13 @@ function multiply(a, b) {
 }
 
 console.log(multiplay(5, 4)); //20
+
+
+//Comum haver comportamentos atípicos ao lidar com parâmetros de funções
+//Podia ocorrer erros assim:
+function multiply(a, b) {
+    return a * b;
+}
+
+console.log(multiply(5)); //NaN  porque esqueceu-se de passar 1 número e a multiplicação foi de 
+                          //5 * undefined(número não definido) 
