fb5aab0f50d705c29b24bd8289b32c065be09239
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-12-01 23:10:21 -0300

    Default Function Arguments - Resolvendo erro do comportamento atípico de || substituindo por operador ternário para validação

--- Aula01_Funcoes-Avancadas-do-ES6/2-default-function-arguments.js
+++ Aula01_Funcoes-Avancadas-do-ES6/2-default-function-arguments.js
@@ -36,3 +36,16 @@ function multiply(a, b) {
 }
 
 console.log(multiply(5, 0)); //Passando 0 como parâmetro para o b    //5   mesmo resultado
+
+
+//Usando a validação de tipo para resolver o problema com o operador lógico ||
+function multiply(a, b) {
+    b = typeof b === undefined ? 1 : b; //Se for undefined atribua 1 senão b
+                                        //Não usa operador lógico e não lê 0 como false mais
+    return a * b;
+} 
+
+console.log(multiply(5, 0)); //0     Assim o comportamento seria como o esperado
+                                    //Se eu omitir 1 número, também irá funcionar
+
+//Porém esse método é verboso
