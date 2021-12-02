f2d6cd6bbb40a98a62e05a24e8fa76c1e8e172b4
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-12-02 15:54:08 -0300

    Enhanced Object Literals - Objeto literal clássico

---
+++ Aula01_Funcoes-Avancadas-do-ES6/3-enhanced-object-literals.js
@@ -0,0 +1,10 @@
+//Enhanced Object Literals
+
+//Maneira clássica de se escrever
+var obj = {
+    prop1: 'Digital Innovation One'
+};
+/*
+- Abre-se chaves
+- Dentro dessas chaves é possível declarar propriedades e métodos
+*/
